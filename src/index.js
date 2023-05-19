855332ab7297b7ff12017f706a23364407e3c7e5
Piquant95 <piquant95@yahoo.com>
2023-05-19 15:11:44 -0400

    deploy 3rd attempt

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { HashRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './pages/App/App';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
+
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
